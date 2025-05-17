95906b43105414dd0ae002da6b6ffd4665e543ed
malazuzia2 <miniaturkazdl@gmail.com>
2025-05-17 05:46:52 +0200

    enemy - zmiana koloru

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Global : MonoBehaviour
 {
@@ -35,31 +36,44 @@ public class Global : MonoBehaviour
         newEnemy.transform.localPosition = new Vector3(538, -512, 0);
         Enemy enemy = newEnemy.GetComponent<Enemy>();
 
+        GameObject activeEnemy = null;
+
         if (random == 0)
         {
             enemy.skeleton.SetActive(true);
+            activeEnemy = enemy.skeleton;
         }
         else if (random == 1)
         {
             enemy.goblin.SetActive(true);
+            activeEnemy = enemy.goblin;
         }
         else if (random == 2)
         {
             enemy.slime.SetActive(true);
+            activeEnemy = enemy.slime;
         }
+
+        // Wybór elementu i koloru
+        Color elementColor = Color.white;
 
         if (randomElement == 0)
         {
             enemy.element = Enemy.Element.Fire;
+            elementColor = Color.red;
         }
         else if (randomElement == 1)
         {
             enemy.element = Enemy.Element.Water;
+            elementColor = Color.blue;
         }
         else if (randomElement == 2)
         {
             enemy.element = Enemy.Element.Earth;
+            elementColor = Color.green;
         }
+
+        SetEnemyColor(activeEnemy, elementColor);
 
         Debug.Log(random);
         Debug.Log(randomElement);
@@ -67,4 +81,19 @@ public class Global : MonoBehaviour
         taskManager.GenerateTasks();
     }
 
+
+    void SetEnemyColor(GameObject enemyObject, Color color)
+    {
+        if (enemyObject == null) return;
+
+        var image = enemyObject.GetComponent<Image>();
+        if (image != null)
+        {
+            image.color = color;
+        }
+        else
+        {
+            Debug.LogWarning("Brak komponentu Image na obiekcie: " + enemyObject.name);
+        }
+    }
 }
